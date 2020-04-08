44c9bed71bc75f469ba108f799ad5d4625a008e5
rahmithamn <rahmithasaid@gmail.com>
2020-04-09 02:28:36 +0800

    Mode List

---
+++ app/src/main/java/com/rmn/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,37 @@
+package com.rmn.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.item_row_hero.view.*
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(hero: Hero) {
+            with(itemView) {
+                Glide.with(itemView.context)
+                    .load(hero.photo)
+                    .apply(RequestOptions().override(55, 55))
+                    .into(img_item_photo)
+
+                tv_item_name.text = hero.name
+                tv_item_decription.text = hero.description
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_hero, viewGroup, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listHero[position])
+    }
+
+}
