9b88c1cb88f32a7015057508517620ba0e5ac0ec
rahmithamn <rahmithasaid@gmail.com>
2020-04-13 14:48:58 +0800

    add Pop Up Behavior

--- app/src/main/java/com/rmn/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/rmn/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_detail_category.*
 
 /**
@@ -26,6 +27,10 @@ class DetailCategoryFragment : Fragment() {
         val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
         category_name.setText(dataName)
         category_description.setText("Stock : $dataDescription")
+
+        btn_profile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
 }
