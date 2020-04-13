1a5d0dd551c647b910fb40dc59345371b8e6d234
rahmithamn <rahmithasaid@gmail.com>
2020-04-13 14:27:44 +0800

    add sent data with bundle

--- app/src/main/java/com/rmn/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/rmn/mynavigation/DetailCategoryFragment.kt
@@ -5,31 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_detail_category.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment() {
+class DetailCategoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btn_category.setOnClickListener (
-            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
-            )
-        btn_profile.setOnClickListener {view ->
-            view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
-        }
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+        category_name.setText(dataName)
+        category_description.setText("Stock : $dataDescription")
     }
 
 }
